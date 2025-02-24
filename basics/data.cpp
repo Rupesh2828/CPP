e8b598efa869e2721461903ea6d063d3fce8a268
Rupesh Jadhav <rupesh.jadhav7700@gmail.com>
2025-02-24 18:43:58 +0530

    basics.

---
+++ basics/data.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int teaLeaves = 50;
+    float waterTemperature = 85.55555;
+    double priceOfTea = 599.9999;
+    char teaType = 'G';
+    bool isTeaReady = 0;
+
+    cout << priceOfTea << endl;
+    cout << waterTemperature << endl;
+
+    return 0;
+    
+}
